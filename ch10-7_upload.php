ea7e4e544882240f15c14a8d6a54ebbdc188232e
NutchanonTangjai <6206021611109@fitm.kmutnb.ac.th>
2021-03-17 09:47:41 +0700

    up

--- ch10-7_upload.php
+++ ch10-7_upload.php
@@ -1,33 +1,25 @@
-<html>
+<?php
+    echo "<br><br>";
+    $ImageFile= $_FILES['ImageFile']['name'];
+    $MAX_FILE_SIZE = $_POST['MAX_FILE_SIZE'];
+    print_r($_FILES);
 
-<head>
-    <title> Upload file </title>
-</head>
-
-<body>
-    <form enctype="multipart/form-data" method="post" action="lab10-7_upload.php">
-        <input type="hidden" name="MAX_FILE_SIZE" value="500000">
-        เลือกไฟล์ที่ต้องการ : <input type="file" name="ImageFile" size="50">
-        <input type="submit" name="Submit" value="Upload Now">
-    </form>
-</body>
-
-</html>
-
-<?php if (isset($ImageFile)) {
-    $ImageFile_name;
-    $ImageFile_type;
-    $ImageFile_size;
+ if (isset($ImageFile)) {
+    $ImageFile_name = $_FILES['ImageFile']['name'];
+    $ImageFile_type= $_FILES['ImageFile']['type'];
+    $ImageFile_size= $_FILES['ImageFile']['size'];
     echo "File name : " . $ImageFile_name . "<br>";
     echo "File type : " . $ImageFile_type . "<br>";
     echo "File size : " . $ImageFile_size . "<br>";
     if ($ImageFile_type == "image/gif" or $ImageFile_type == "image/jpeg") {
         if ($ImageFile_size <= $MAX_FILE_SIZE) {
-            copy($ImageFile, "pictures/$ImageFile_name");
-            unlink($ImageFile);
+
+            copy($_FILES['ImageFile']['tmp_name'], "$ImageFile_Name");
+            unlink($ImageFile_Name);
+            //move_uploaded_file($_FILES['ImageFile']['tmp_name'],$ImageFile);
             echo "<li>บันทึกข้อมูลเรียบร้อย<br>";
             echo  "<a href='ch10-7_form.php'>กลับไป upload </a><br>";
-            echo  "<image src='pictures/$ImageFile_name' width='320' height='240'>";
+            echo  "<image src='$ImageFile' width='320' height='240'>";
         } else {
             echo "<li>รูปภาพมีขนาดใหญ่กว่า 500 kb.<br>";
             echo "<input type=\"button\" value=\"กลับไปแก้ไข\" ";
@@ -38,4 +30,4 @@
         echo "<input type=\"button\" value=\"กลับไปแก้ไข\" ";
         echo "onclick= \"history.back();\" style=\"cursor:hand\">";
     }
-} ?>
+}
